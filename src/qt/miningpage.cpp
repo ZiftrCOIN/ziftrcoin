1ba5fc65c65f18b802a482a65db8cfafa48e22e6
Stephen <scmorse@colby.edu>
2015-03-12 21:10:43 -0400

    Default pool address saved using the account feature

--- src/qt/miningpage.cpp
+++ src/qt/miningpage.cpp
@@ -11,6 +11,22 @@
 
 extern json_spirit::Value GetNetworkHashPS(int lookup, int height);
 
+static const string introText = 
+    "Welcome to ziftrCOIN mining!\n"
+    "If all you would like to do is to just mine to the ziftrCOIN pool, \n"
+    "then just click 'Start Mining'!\n"
+    "\n"
+    "Please occasionally check that the cumulative hashrate of the pool you are \n"
+    "in does not have more than 50\% of the network hashrate. It is not healty \n"
+    "for the hashing power of the network to be so concentrated. \n"
+    "\n"
+    "You can also set the following parameters in your ziftrcoin.conf file to set \n"
+    "and automatically load your chosen pool configurations at start. \n"
+    "  poolserver=\n"
+    "  poolport=\n"
+    "  poolusername=\n"
+    "  poolpassword=\n\n";
+
 static QString formatHashrate(int64_t n)
 {
     if (n == 0)
@@ -55,9 +71,27 @@ MiningPage::MiningPage(QWidget *parent) :
 
     initThreads = 0;
 
+    this->AddListItem(QString(introText.c_str()));
+
     ui->serverLine->setText(QString(GetArg("-poolserver", "stratum+tcp://ziftrpool.io").c_str()));
     ui->portLine->setText(QString(GetArg("-poolport", "3032").c_str()));
-    ui->usernameLine->setText(QString(GetArg("-poolusername", "").c_str()));
+    
+    string sPoolUsername = GetArg("-poolusername", "");
+    
+    if (sPoolUsername.empty())
+    {
+        // If getaccountaddress fails due to not having enough addresses in key pool,
+        // just don't autofill
+        try {
+            json_spirit::Array params;
+            params.push_back(string("Ziftr Pool Payouts"));
+            params.push_back(true);
+            sPoolUsername = getaccountaddress(params, false).get_str();
+        } 
+        catch (exception& e) {}
+    }
+    ui->usernameLine->setText(QString(sPoolUsername.c_str()));
+
     ui->passwordLine->setText(QString(GetArg("-poolpassword", "").c_str()));
 
     connect(readTimer, SIGNAL(timeout()), this, SLOT(readProcessOutput()));
@@ -133,6 +167,8 @@ void MiningPage::startPoolMining()
     QString urlLine = QString("%1:%2").arg(url, ui->portLine->text());
     QString userLine = QString("%1").arg(ui->usernameLine->text());
     QString passwordLine = QString("%1").arg(ui->passwordLine->text());
+    if (passwordLine.isEmpty())
+        passwordLine = QString("x");
 
     args << "--algo" << "ziftr";
     args << "--url" << urlLine.toAscii();
@@ -164,7 +200,7 @@ void MiningPage::startPoolMining()
 
     if (ui->debugCheckBox->isChecked())
     {
-        ui->list->addItem(QString("Using minerd application located at: ").append(program));
+        this->AddListItem(QString("Using minerd application located at: ").append(program));
     }
 
     ui->mineSpeedLabel->setText("Your hash rate: N/A");
@@ -231,7 +267,7 @@ void MiningPage::readProcessOutput()
 
             if (ui->debugCheckBox->isChecked())
             {
-                ui->list->addItem(line.trimmed());
+                this->AddListItem(line.trimmed());
             }
             ui->list->scrollToBottom();
 
@@ -316,7 +352,7 @@ void MiningPage::minerFinished()
         reportToList("Solo mining stopped.", ERROR, NULL);
     else
         reportToList("Miner exited.", ERROR, NULL);
-    ui->list->addItem("");
+    this->AddListItem("");
     minerActive = false;
     resetMiningButton();
     clientmodel->setMining(getMiningType(), false, -1);
@@ -376,7 +412,7 @@ void MiningPage::reportToList(QString msg, int type, QString time)
     else
         message = QString("[%1] - %2").arg(time, msg);
 
-    ui->list->addItem(message);
+    this->AddListItem(message);
 
     switch(type)
     {
@@ -405,6 +441,13 @@ void MiningPage::reportToList(QString msg, int type, QString time)
 
 
     ui->list->scrollToBottom();
+}
+
+void MiningPage::AddListItem(const QString& text)
+{
+    QListWidgetItem * item = new QListWidgetItem(text);
+    item->setFlags(item->flags() | Qt::ItemIsSelectable);
+    ui->list->addItem(item);
 }
 
 // Function for fetching the time
@@ -486,6 +529,12 @@ void MiningPage::changePercentMiningPower(int i)
 void MiningPage::resetMiningButton()
 {
     ui->startButton->setText(minerActive ? "Stop Mining" : "Start Mining");
+    QString style;
+    if (minerActive)
+        style = "QPushButton { color: #e46e1f; }";
+    else 
+        style = "QPushButton { color: #15444A; }";
+    ui->startButton->setStyleSheet(style);
     EnableMiningControlsAppropriately();
 }
 
@@ -499,8 +548,8 @@ void MiningPage::logShareCounts()
 
     QString messageTotal = QString("Total Shares Accepted: %1 - Rejected: %2").arg(acceptedString, rejectedString);
     QString messageShare = QString("Round Shares Accepted: %1 - Rejected: %2").arg(roundAcceptedString, roundRejectedString);
-    ui->list->addItem(messageTotal);
-    ui->list->addItem(messageShare);
+    this->AddListItem(messageTotal);
+    this->AddListItem(messageShare);
 }
 
 // static QString formatTimeInterval(CBigNum t)
